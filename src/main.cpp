3c0c4f5518b93d80422356c8dafe3041a83283ff
Jay Kittler <94416018+JayKitt28@users.noreply.github.com>
2025-09-02 12:46:47 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,6 @@ int main()
 	std::cout << "a + b = " << MathUtils::add(3, 4) << std::endl;
 	std::cout << "a * b = " << MathUtils::multiply(3, 4) << std::endl;
 	std::cout << "square(a) = " << AdvancedMath::square(3) << std::endl;
+	return 0;
 }
 
